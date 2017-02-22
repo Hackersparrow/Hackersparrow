4f22e96208e4fb78b9d0be3298c9591067462017
Adrian Diaz <adriandiazmancera@hotmail.com>
2017-02-22 11:01:23 +0100

    terminar de hacer activity, corregidos los segundos y testeado en movil.

--- app/src/main/java/com/hackersparrow/hackersparrow/Activities/SplashScreen.java
+++ app/src/main/java/com/hackersparrow/hackersparrow/Activities/SplashScreen.java
@@ -1,16 +1,50 @@
 package com.hackersparrow.hackersparrow.Activities;
+import java.util.Timer;
+import java.util.TimerTask;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.Window;
 
+import com.hackersparrow.hackersparrow.MainActivity;
 import com.hackersparrow.hackersparrow.R;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends Activity {
+
+    // Set the duration of the splash screen
+    private static final long SPLASH_SCREEN_DELAY = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set portrait orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // Hide title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_splash_screen);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                // Start the next activity
+                Intent mainIntent = new Intent().setClass(
+                        SplashScreen.this, MainActivity.class);
+                startActivity(mainIntent);
+
+                // Close the activity so the user won't able to go back this
+                // activity pressing Back button
+                finish();
+            }
+        };
+
+        // Simulate a long loading process on application startup.
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
     }
+
 }
-tools:context="com.hackersparrow.hackersparrow.Activities.SplashScreen">
