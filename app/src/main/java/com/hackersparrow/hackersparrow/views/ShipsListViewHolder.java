74d7a25bac1d7766f7100d71decf854ff8d01507
Saúl Marín Querido <saul.appdev@gmail.com>
2017-02-22 10:14:35 +0100

    Coded ShipsListViewHolder.class. Added row_ship.xml with a layout sample.

--- app/src/main/java/com/hackersparrow/hackersparrow/views/ShipsListViewHolder.java
+++ app/src/main/java/com/hackersparrow/hackersparrow/views/ShipsListViewHolder.java
@@ -1,5 +1,37 @@
 package com.hackersparrow.hackersparrow.views;
 
 
-public class ShipsListViewHolder {
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.hackersparrow.hackersparrow.R;
+import com.hackersparrow.hackersparrow.model.Ship;
+
+public class ShipsListViewHolder extends RecyclerView.ViewHolder{
+
+    private Ship ship;
+    private ImageView shipImage;
+    private TextView shipName;
+    private TextView shipCapability;
+
+    public ShipsListViewHolder(View itemView) {
+        super(itemView);
+        shipImage = (ImageView) itemView.findViewById(R.id.row_ship___ship_image);
+        shipName = (TextView) itemView.findViewById(R.id.row_ship___ship_name);
+        shipCapability = (TextView) itemView.findViewById(R.id.row_ship___ship_price);
+
+        shipName.setText(ship.getName());
+        shipCapability.setText(ship.getCapability());
+    }
+
+    public Ship getShip() {
+        return ship;
+    }
+
+    public void setShip(Ship ship) {
+        this.ship = ship;
+    }
+
 }
