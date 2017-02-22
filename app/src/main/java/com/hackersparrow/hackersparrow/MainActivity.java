41736c515892f616ace92ad4fb3905dd85416b5d
Adrian Diaz <adriandiazmancera@hotmail.com>
2017-02-22 09:57:16 +0100

    crear carpeta activities y la activitie de Splas screen

--- app/src/main/java/com/hackersparrow/hackersparrow/MainActivity.java
+++ app/src/main/java/com/hackersparrow/hackersparrow/MainActivity.java
@@ -1,7 +1,7 @@
 package com.hackersparrow.hackersparrow;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
