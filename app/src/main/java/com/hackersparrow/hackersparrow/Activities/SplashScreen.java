4ede350dc5f3c81ea61eaf2a361bdc057fae0b1a
Adrian Diaz <adriandiazmancera@hotmail.com>
2017-02-22 10:41:32 +0100

    crear directorio drawable, pasar el logo y he hecho el xml

--- app/src/main/java/com/hackersparrow/hackersparrow/Activities/SplashScreen.java
+++ app/src/main/java/com/hackersparrow/hackersparrow/Activities/SplashScreen.java
@@ -3,6 +3,8 @@ package com.hackersparrow.hackersparrow.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.hackersparrow.hackersparrow.R;
+
 public class SplashScreen extends AppCompatActivity {
 
     @Override
@@ -11,3 +13,4 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
     }
 }
+tools:context="com.hackersparrow.hackersparrow.Activities.SplashScreen">
