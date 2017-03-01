fe76c4e680629b6cff53ca306484a5b8a2c1019f
smappdev <saul.appdev@gmail.com>
2017-03-01 19:30:38 +0100

    Changed color and Title to UserInfoActivity

--- app/src/main/java/com/hackersparrow/hackersparrow/activities/UserInfoActivity.java
+++ app/src/main/java/com/hackersparrow/hackersparrow/activities/UserInfoActivity.java
@@ -1,5 +1,7 @@
 package com.hackersparrow.hackersparrow.activities;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,6 +12,11 @@ public class UserInfoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#0096C8"));
+        getSupportActionBar().setBackgroundDrawable(colorDrawable);
+        getSupportActionBar().setTitle("Solicitar información");
+
         setContentView(R.layout.activity_user_info);
     }
 }
