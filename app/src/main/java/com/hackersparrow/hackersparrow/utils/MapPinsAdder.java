4e078e1a03db7c17abaa349c7e4381ce8b9e6ee2
smappdev <saul.appdev@gmail.com>
2017-03-01 17:04:38 +0100

    Added description snippet to markers

--- app/src/main/java/com/hackersparrow/hackersparrow/utils/MapPinsAdder.java
+++ app/src/main/java/com/hackersparrow/hackersparrow/utils/MapPinsAdder.java
@@ -30,7 +30,7 @@ public class MapPinsAdder {
             final String profileImageUrl = pinnable.getPinImageUrl();
 
             final MarkerOptions marker = new MarkerOptions().position(position).icon(BitmapDescriptorFactory.fromResource(R.drawable.chincheta45px_2)).title(pinnable.getPinDescription());
-
+            marker.snippet("Pulsa para acceder");
             Marker m = googleMap.addMarker(marker);
             m.setTag(pinnable);
         }
