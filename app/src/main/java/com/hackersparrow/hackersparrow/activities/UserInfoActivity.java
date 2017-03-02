0395fc9946b56fe7f95e93a610c550743dfb1f38
jmajyo-por <jmajyo@gmail.com>
2017-03-02 19:59:46 +0100

    added snackbar

--- app/src/main/java/com/hackersparrow/hackersparrow/activities/UserInfoActivity.java
+++ app/src/main/java/com/hackersparrow/hackersparrow/activities/UserInfoActivity.java
@@ -1,15 +1,17 @@
 package com.hackersparrow.hackersparrow.activities;
 
+import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.hackersparrow.hackersparrow.R;
 
 public class UserInfoActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +22,8 @@ public class UserInfoActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.toolbar);
         setContentView(R.layout.activity_user_info);
+
+        View rootView = findViewById(android.R.id.content);
+        Snackbar.make(rootView, "Prueba", Snackbar. LENGTH_LONG).show();
     }
 }
