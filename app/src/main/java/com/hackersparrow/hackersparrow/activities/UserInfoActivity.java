afac714f57e2c9405da0d534a0c763a15b84e72b
smappdev <saul.appdev@gmail.com>
2017-03-02 19:42:25 +0100

    Refactor of intents. They send/get Ship object till UserInfoActivity

--- app/src/main/java/com/hackersparrow/hackersparrow/activities/UserInfoActivity.java
+++ app/src/main/java/com/hackersparrow/hackersparrow/activities/UserInfoActivity.java
@@ -1,5 +1,6 @@
 package com.hackersparrow.hackersparrow.activities;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -7,12 +8,16 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 import com.hackersparrow.hackersparrow.R;
+import com.hackersparrow.hackersparrow.model.Ship;
 
 public class UserInfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        Ship ship = (Ship) intent.getSerializableExtra("selected_ship");
+        System.out.println(ship.getId() + " " + ship.getName());
 
         ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#0096C8"));
         getSupportActionBar().setBackgroundDrawable(colorDrawable);
