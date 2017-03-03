ce021d9c0ada685e705b15d6291a563292baa5ac
smappdev <saul.appdev@gmail.com>
2017-03-03 20:39:52 +0100

    Splash Has Blinking text that shows connection error

--- app/src/main/java/com/hackersparrow/hackersparrow/Activities/SplashScreen.java
+++ app/src/main/java/com/hackersparrow/hackersparrow/Activities/SplashScreen.java
@@ -8,16 +8,23 @@ import android.graphics.PixelFormat;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import com.hackersparrow.hackersparrow.R;
 
 import net.bohush.geometricprogressview.GeometricProgressView;
 
+import java.sql.SQLOutput;
+
 public class SplashScreen extends Activity {
     public static Activity maps;
+    private TextView errorText;
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
         Window window = getWindow();
@@ -29,6 +36,9 @@ public class SplashScreen extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        errorText = (TextView) findViewById(R.id.splash_error_text);
+
         maps=this;
         StartAnimations();
     }
@@ -56,12 +66,29 @@ public class SplashScreen extends Activity {
             public void run() {
                 try {
                     int waited = 0;
+                    int noConnectionTotal = 0;
                     // Splash screen pause time
-                    while (waited < 5000) {
+                    while (waited < 4000) {
                         sleep(100);
                         waited += 100;
                         if (!isNetworkAvailable(getBaseContext())){
+                            noConnectionTotal = noConnectionTotal + 100;
                             waited = 0;
+                            if (noConnectionTotal >= 10000){
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        errorText.setVisibility(View.VISIBLE);
+                                        Animation anim = new AlphaAnimation(0.0f, 1.0f);
+                                        anim.setDuration(400); //You can manage the blinking time with this parameter
+                                        anim.setStartOffset(20);
+                                        anim.setRepeatMode(Animation.REVERSE);
+                                        anim.setRepeatCount(Animation.INFINITE);
+                                        errorText.startAnimation(anim);
+                                    }
+                                });
+                                noConnectionTotal = 0;
+                            }
                         }
 
                     }
