a15751be83003439c18bf98e73d0ce2a5309350e
Saúl Marín Querido <saul.appdev@gmail.com>
2017-03-05 12:02:53 +0100

    Added Background to UserInfoActivity

--- app/src/main/java/com/hackersparrow/hackersparrow/activities/UserInfoActivity.java
+++ app/src/main/java/com/hackersparrow/hackersparrow/activities/UserInfoActivity.java
@@ -3,6 +3,8 @@ package com.hackersparrow.hackersparrow.activities;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
@@ -10,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import com.hackersparrow.hackersparrow.R;
 import com.hackersparrow.hackersparrow.model.Ship;
@@ -42,6 +45,16 @@ public class UserInfoActivity extends AppCompatActivity {
         emailText = (TextView) findViewById(R.id.info_email);
         phoneText = (TextView) findViewById(R.id.info_phone);
         sendButton = (Button) findViewById(R.id.info_send_button);
+        VideoView videoview = (VideoView) findViewById(R.id.videoView);
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.intro);
+        videoview.setVideoURI(uri);
+        videoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
+            }
+        });
+        videoview.start();
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
